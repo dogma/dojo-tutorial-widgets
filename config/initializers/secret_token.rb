5dcb1fd0465e3fd66b22f59dc6d331bc182d3b5b
Gerwood Stewart <gerwood@gmail.com>
2011-05-19 10:54:58 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DojoTodo::Application.config.secret_token = '8c7ea14e2c5018a2d99dcc7d318e7bd259d6377515a056cef4bd11ecbe5f5bf6212e257eb1f5789b03632e8b96e16014ee589ea0bb87d7a3aed474fb762d6c28'
